11b85c0a19f4033c7c043c526e4e618d9833737f
liangyan23 <liangyan23@meituan.com>
2024-06-11 12:58:22 +0800

    commit: modify github advisories and modify code logic

--- Tests/SearcherTests/githubSearcherTest.py
+++ Tests/SearcherTests/githubSearcherTest.py
@@ -1,6 +1,8 @@
 import unittest
 from ExternalSearchers import github_searcher
+from ExternalSearchers.github_searcher import graphql_search
 from Tests.Utils.test_utils import generate_testcases
+from Tests.Utils.advisories_utils import generate_url_list_for_github_advisories, generate_testcases_for_advisories
 
 
 class TestGithubSearcher(unittest.TestCase):
@@ -10,13 +12,15 @@ class TestGithubSearcher(unittest.TestCase):
                            'https://github.com/advisories?query=CVE-2022-21188',  # 搜索不到
                            'https://github.com/advisories?query=CVE-2024-28103',  # 单个匹配
                            'https://github.com/advisories?query=GHSA-5cxf-xx9j-54jc',  # 单个匹配
-                           'https://github.com/advisories?query=CVE-2024-4886 '  # 单个匹配
                            ]
         advisories_testcases = []
         generate_testcases(advisories_list, advisories_testcases, github_searcher.advisories_search_by_url)
 
     def test_search_advisories(self):
-        github_searcher.advisories_search()
+        parsed_data = github_searcher.advisories_search()
+        url_list = generate_url_list_for_github_advisories()
+        advisories_testcases = []
+        generate_testcases_for_advisories(url_list, advisories_testcases, parsed_data)
 
     if __name__ == '__main__':
         unittest.main()
