0bde9bba83c62bf3d4a882b6ca965736e2acca5a
blackman <522023320102@smail.nju.edu.cn>
2024-10-22 16:26:56 +0800

    commit：修改es和mongo的链接为容器化后的

--- Constants/dbConstants.py
+++ Constants/dbConstants.py
@@ -1,10 +1,10 @@
 from elasticsearch import Elasticsearch
 from pymongo import MongoClient
 
-es_url = "http://localhost:9200"
-mongo_url = "mongodb://localhost:27017"
-# es_url = 'http://elasticsearch:9200'
-# mongo_url = "mongodb://mongodb:27017/"
+#es_url = "http://localhost:9200"
+#mongo_url = "mongodb://localhost:27017"
+es_url = 'http://elasticsearch:9200'
+mongo_url = "mongodb://mongodb:27017/"
 
 es_username = 'elastic'
 es_password = '123456'
